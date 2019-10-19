3b955ba146dc767ebcc858656222b6ea4b244217
gyj <1401357358@qq.com>
2019-10-19 13:53:10 +0800

    cube-ui安装完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import './cube-ui'
+import './cube-ui'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
